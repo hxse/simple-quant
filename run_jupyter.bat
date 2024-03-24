ae46481a137371f9021d5eba38e9eaab4728c3e9
hxse <narutohy@163.com>
2024-03-24 17:30:55 +0800

    update

--- run_jupyter.bat
+++ run_jupyter.bat
@@ -1,3 +1,3 @@
-pdm run python -m ipykernel install --user --name .venv
-pdm run pip install ipykernel anywidget
+@REM pdm run python -m ipykernel install --user --name .venv
+@REM pdm run pip install ipykernel anywidget
 pdm run jupyter notebook
